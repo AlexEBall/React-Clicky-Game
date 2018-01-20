21b13821f998fc95f4ce93f0eea653e5cc6fa30e
Alex Edward Ball <ballalexedward@gmail.com>
2018-01-20 11:38:39 -0800

    backstretch added

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	$.backstretch([
+          "assets/images/Lz1VBIH.png",
+          "assets/images/JBBIvIr.png",
+          "assets/images/NVSPDIX.png",
+          "assets/images/PqhM60k.png",
+          "assets/images/UnSC4tP.png",
+          "assets/images/VnCp5xG.png",
+          "assets/images/PQGcPIn.png",
+          "assets/images/X5sQYNy.png",
+          "assets/images/vRKf0IE.png",
+          "assets/images/5gYdXIh.png",
+          "assets/images/OhuSXTl.png",
+        ], {
+            duration: 12000,
+            fade: 900
+        });
+
+  console.log('here');
+});
